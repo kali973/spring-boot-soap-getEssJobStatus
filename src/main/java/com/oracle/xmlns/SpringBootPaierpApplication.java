8db891ab2afa9cb16d91a475802cee95d437031f
coutard <gilles.coutard@telecom-paris.fr>
2022-09-09 08:43:54 -0400

    Initialisation SOAP Spring Boot - Java

--- src/main/java/com/oracle/xmlns/SpringBootPaierpApplication.java
+++ src/main/java/com/oracle/xmlns/SpringBootPaierpApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootPaierpApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPaierpApplication.class, args);
     }
